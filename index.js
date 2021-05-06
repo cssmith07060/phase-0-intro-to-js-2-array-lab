6cabdffb6aa7552c2fabb869981b2c62bdab3304
cssmith07060 <cssmith07060@gmail.com>
2021-05-06 15:21:46 -0400

    cssmith07060/phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
 // Write your solution here!
+
+
+//Run the tests using npm test.
+//Read the errors; vocalize what they're asking you to do.
+//Write code; repeat steps 1 and 2 often until a test passes.
+//Repeat as needed for the remaining tests.
+
+var cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat(){
+  cats.pop();
+  return cats;
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift()
+  return cats
+}
+function prependCat(name) {
+  //clone cats array
+  var catsCopy = [...cats]
+  catsCopy.unshift(name)
+  return catsCopy;
+}
+
+function destructivelyRemoveCat(){
+  cats.shift();
+  return cats;
+}
+
+function appendCat(name){
+  var name_1= [...cats,name];
+  return name_1;
+}
+
+function prependcats(name){
+  var name_2=[name, ...cats];
+  return name_2
+}
+
+function removeLastCat(){
+  var caats= cats.slice(0,cats.length-1);
+  return caats;
+}
+
+function removeFirstCat(){
+  var catts= cats.slice(1)
+  return catts
+}
+ 
+
+
+
+
+
+
+
+
+
+
